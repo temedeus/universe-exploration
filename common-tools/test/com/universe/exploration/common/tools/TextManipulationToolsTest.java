f1a7cd9d88fd24ecc9aa3d8d6ba4963fcdb87178
temedeus <puurunenteemu@hotmail.com>
2018-09-30 12:51:50 +0300

    Started working on fixing unit tests

--- common-tools/test/com/universe/exploration/common/tools/TextManipulationToolsTest.java
+++ common-tools/test/com/universe/exploration/common/tools/TextManipulationToolsTest.java
@@ -1,22 +1,22 @@
 package com.universe.exploration.common.tools;
 
-import java.util.ArrayList;
-
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 public class TextManipulationToolsTest {
 
     @Test
     public void testImplodeListAsStringJoinsStringsProperlyWithSeparator() throws Exception {
-	ArrayList<String> testStringList = new ArrayList<String>();
-	testStringList.add("blaa");
-	testStringList.add("bloo");
-	testStringList.add("blee");
+        ArrayList<String> testStringList = new ArrayList<>();
+        testStringList.add("blaa");
+        testStringList.add("bloo");
+        testStringList.add("blee");
 
-	String joined = TextManipulationTools.implodeListAsString(testStringList, ", ");
-
-	Assert.assertEquals("blaa, bloo, blee", joined);
+        String joined = TextManipulationTools.implodeListAsString(testStringList, ", ");
+        System.out.println("bkporgrkpo");
+        Assert.assertEquals("blaa, bloo, blee", joined);
     }
 
 }
