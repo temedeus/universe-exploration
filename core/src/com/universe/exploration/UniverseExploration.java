c6cb6753cea3cc8fcba73da2a27ef428ed2ee27c
Teemu Puurunen <tpuurune@gmail.com>
2021-04-27 22:40:10 +0300

    Batch work.

--- core/src/com/universe/exploration/UniverseExploration.java
+++ core/src/com/universe/exploration/UniverseExploration.java
@@ -5,30 +5,31 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.universe.exploration.screens.MainMenuScreen;
 
-
+/**
+ * Main game class.
+ */
 public class UniverseExploration extends Game {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("images/planets/earthlike.png");
-	}
+    private SpriteBatch batch;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        this.setScreen(new MainMenuScreen(this));
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
+
+	public SpriteBatch getBatch() {
+		return batch;
 	}
 }
--- core/src/com/universe/exploration/screens/MainMenuScreen.java
+++ core/src/com/universe/exploration/screens/MainMenuScreen.java
@@ -1,13 +1,22 @@
 package com.universe.exploration.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.universe.exploration.UniverseExploration;
 
 public class MainMenuScreen implements Screen {
-    UniverseExploration universeExploration;
-    MainMenuScreen(UniverseExploration universeExploration) {
+    Texture img;
+
+    private UniverseExploration universeExploration;
+
+    public MainMenuScreen(final UniverseExploration universeExploration) {
         this.universeExploration = universeExploration;
+        img = new Texture("images/planets/earthlike.png");
     }
+
     @Override
     public void show() {
 
@@ -15,7 +24,11 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void render(float delta) {
-
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        universeExploration.getBatch().begin();
+        universeExploration.getBatch().draw(img, 0, 0);
+        universeExploration.getBatch().end();
     }
 
     @Override
@@ -40,6 +53,6 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        img.dispose();
     }
 }
